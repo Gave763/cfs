2bd28cbcb58ee118b5aee9b14e8063aa81e284fb
Gave763 <gavenathan@yahoo.com>
2013-06-07 19:53:29 -0600

    Create cfs.cpp

---
+++ cfs.cpp
@@ -0,0 +1,21 @@
+/* CFS PROJECT- UNDER CONSTRUCTION
+Cryptographica File Security v.1.0
+*/
+
+#include <windows.h> //
+#include <math.h> //pow, floor, cos
+#include <time.h> //time
+#include <stdlib.h> // rand()
+#include <iostream> // std::cout, std::cin
+#include <fstream> //ifstream class, open, close
+#include <string> //std::string
+#include <exception> //exception class
+#include <map> //map class
+#include <memory> //include allocators
+
+#define ROL(x,shift,size)
+int main(int argc, char* argv) {
+    if(!argc | argv[0] = '/?') {
+        cout << help; //help
+    }
+}
